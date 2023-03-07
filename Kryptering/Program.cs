b0bbb79d6101bc330fdcbd49f20d9c145ca0c604
camkurup <camkurup@hotmail.com>
2023-03-07 11:01:28 +0100

    -

--- Kryptering/Program.cs
+++ Kryptering/Program.cs
@@ -1,6 +1,7 @@
-﻿Kryptering.Encrypter encrypter= new Kryptering.Encrypter();
+﻿Kryptering.Encrypter encrypter = new Kryptering.Encrypter();
+Kryptering.Tilfældigheder tilfældigheder = new Kryptering.Tilfældigheder();
 
-
+//Encryt and Decrypt
 var input = Console.ReadLine();
 var incrpyted = encrypter.Encrypt(input);
 var decrypting = encrypter.Decrypt(incrpyted);
@@ -8,3 +9,6 @@ var decrypting = encrypter.Decrypt(incrpyted);
 Console.WriteLine("Krypteret text: " + incrpyted);
 Console.WriteLine("Dekrypteret text: " + decrypting);
 
+//Randomeness
+var rndExecute = tilfældigheder.Execute(10000, 100);
+Console.WriteLine(rndExecute);
---
+++ Kryptering/Tilfældigheder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Kryptering
+{
+    internal class Tilfældigheder
+    {
+        public int Execute(int maxRandom, int randomCount)
+        {
+            var rnd = 0;
+            for (int i = 0; i < randomCount; i++)
+            {
+                rnd = RandomNumberGenerator.GetInt32(0, maxRandom);
+            }
+            return rnd;
+        }
+    }
+}
